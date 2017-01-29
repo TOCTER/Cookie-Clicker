0719bf096014b13745e04c9ad0d7565cec0d5b27
TOCTER <H.Warner02@cityoflondonschool.org.uk>
2017-01-29 10:09:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ var cookieQuantity = 0;
 
 $('.cookieQuantity').on('click', function() {
   var cookieQuantityElement = document.getElementBydId('cookieQuantity');
+  console.log(cookieQuantity+1)
 }
